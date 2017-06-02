d18713db8363aacdb8729004db6691776d9bfe52
Ju Yun Kim <kimj4@carleton.edu>
2017-06-02 17:40:38 -0500

    fixed command line arguments

--- playGame.py
+++ playGame.py
@@ -4,7 +4,7 @@ import sys
 
 def playPlayers(playersFile, playerIndex, goFirst, symmetry, hyper):
     substrate = hcf.configureSubstrate
-    with open('playersFile', 'r') as f:
+    with open(playersFile, 'r') as f:
         listOfPlayers = pickle.load(f)
     if goFirst:
         play(listOfPlayers[playerIndex], "Human", substrate, False, True, False, GLOBAL_DEPTH)
@@ -12,19 +12,20 @@ def playPlayers(playersFile, playerIndex, goFirst, symmetry, hyper):
         play(listOfPlayers[playerIndex], "Human", substrate, False, True, False, GLOBAL_DEPTH)
 
 def main():
-    if (len(sys.argv) < 3):
+    if (len(sys.argv) < 6):
         print("Arguments expected")
         print("path to file containing players")
         print("the index of player you want to play in the list of players")
         print("1 if you want to go first, 0 if player goes first")
         print("1 if player used symmetry, 0 else")
         print("1 if player is HyperNEAT, 0 else")
+        return
 
     playersFile = sys.argv[1]
-    playerIndex = sys.argv[2]
-    goFirst = sys.argv[3]
-    symmetry = sys.argv[4]
-    hyper = sys.argv[4]
+    playerIndex = int(sys.argv[2])
+    goFirst = int(sys.argv[3])
+    symmetry = int(sys.argv[4])
+    hyper = int(sys.argv[4])
 
     playPlayers(playersFile, playerIndex, goFirst, symmetry, hyper)
 
