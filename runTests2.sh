07ea1dbefd2ee4cc213053a9d12678de0dcaba5d
Ju Yun Kim <kimj4@carleton.edu>
2017-06-01 20:31:46 -0500

    added more testing scripts

---
+++ runTests2.sh
@@ -0,0 +1,15 @@
+python hcf.py 000_3.txt 0 0 0 000_3g.txt 20
+python hcf.py 000_4.txt 0 0 0 000_4g.txt 20
+python hcf.py 000_5.txt 0 0 0 000_5g.txt 20
+
+python hcf.py 100_3.txt 1 0 0 100_3g.txt 20
+python hcf.py 100_4.txt 1 0 0 100_4g.txt 20
+python hcf.py 100_5.txt 1 0 0 100_5g.txt 20
+
+python hcf.py 001_3.txt 0 0 0 001_3g.txt 20
+python hcf.py 001_4.txt 0 0 0 001_4g.txt 20
+python hcf.py 001_5.txt 0 0 0 001_5g.txt 20
+
+python hcf.py 101_3.txt 1 0 0 101_3g.txt 20
+python hcf.py 101_4.txt 1 0 0 101_4g.txt 20
+python hcf.py 101_5.txt 1 0 0 101_5g.txt 20
