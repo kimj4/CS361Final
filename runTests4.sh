e53455812db884619dd8a9c6f65cd230a28cddfc
juyun <kimj4@carleton.edu>
2017-06-02 15:04:32 -0500

    added data

---
+++ runTests4.sh
@@ -0,0 +1,15 @@
+python hcf.py 000_9.txt 0 0 0 000_9g.txt 20
+python hcf.py 000_10.txt 0 0 0 000_10g.txt 20
+python hcf.py 000_11.txt 0 0 0 000_11g.txt 20
+
+python hcf.py 100_9.txt 1 0 0 100_9g.txt 20
+python hcf.py 100_10.txt 1 0 0 100_10g.txt 20
+python hcf.py 100_11.txt 1 0 0 100_11g.txt 20
+
+python hcf.py 001_9.txt 0 0 0 001_9g.txt 20
+python hcf.py 001_10.txt 0 0 0 001_10g.txt 20
+python hcf.py 001_11.txt 0 0 0 001_11g.txt 20
+
+python hcf.py 101_9.txt 1 0 0 101_9g.txt 20
+python hcf.py 101_10.txt 1 0 0 101_10g.txt 20
+python hcf.py 101_11.txt 1 0 0 101_11g.txt 20
---
+++ runTests6.sh
@@ -0,0 +1,15 @@
+python hcf.py 000_15.txt 0 0 0 000_15g.txt 20
+python hcf.py 000_16.txt 0 0 0 000_16g.txt 20
+python hcf.py 000_17.txt 0 0 0 000_17g.txt 20
+
+python hcf.py 100_15.txt 1 0 0 100_15g.txt 20
+python hcf.py 100_16.txt 1 0 0 100_16g.txt 20
+python hcf.py 100_17.txt 1 0 0 100_17g.txt 20
+
+python hcf.py 001_15.txt 0 0 0 001_15g.txt 20
+python hcf.py 001_16.txt 0 0 0 001_16g.txt 20
+python hcf.py 001_17.txt 0 0 0 001_17g.txt 20
+
+python hcf.py 101_15.txt 1 0 0 101_15g.txt 20
+python hcf.py 101_16.txt 1 0 0 101_16g.txt 20
+python hcf.py 101_17.txt 1 0 0 101_17g.txt 20
