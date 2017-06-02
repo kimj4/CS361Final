713173e711da7d0d95fee932203d0a72eb466403
kimj4 <kimj4@carleton.edu>
2017-06-02 11:37:41 -0500

    Update runTests3.sh

--- runTests3.sh
+++ runTests3.sh
@@ -6,10 +6,10 @@ python hcf.py 100_6.txt 1 0 0 100_6g.txt 20
 python hcf.py 100_7.txt 1 0 0 100_7g.txt 20
 python hcf.py 100_8.txt 1 0 0 100_8g.txt 20
 
-python hcf.py 001_6.txt 0 0 0 001_6g.txt 20
-python hcf.py 001_7.txt 0 0 0 001_7g.txt 20
-python hcf.py 001_8.txt 0 0 0 001_8g.txt 20
+python hcf.py 001_6.txt 0 0 1 001_6g.txt 20
+python hcf.py 001_7.txt 0 0 1 001_7g.txt 20
+python hcf.py 001_8.txt 0 0 1 001_8g.txt 20
 
-python hcf.py 101_6.txt 1 0 0 101_6g.txt 20
-python hcf.py 101_7.txt 1 0 0 101_7g.txt 20
-python hcf.py 101_8.txt 1 0 0 101_8g.txt 20
+python hcf.py 101_6.txt 1 0 1 101_6g.txt 20
+python hcf.py 101_7.txt 1 0 1 101_7g.txt 20
+python hcf.py 101_8.txt 1 0 1 101_8g.txt 20
