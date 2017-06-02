3389a77e6a052b083ee5f9e965e58f1360574cf1
Ju Yun Kim <kimj4@carleton.edu>
2017-06-02 16:52:54 -0500

    added data and another script

---
+++ makePlayers.sh
@@ -0,0 +1,7 @@
+python hcf.py NEAT_No_Symmetry.txt 0 0 0 NEAT_No_Symmetry_genome.txt 19
+
+python hcf.py NEAT_Symmetry.txt 1 0 0 NEAT_Symmetry_genome.txt 19
+
+python hcf.py HyperNEAT_No_Symmetry.txt 0 0 1 HyperNEAT_No_Symmetry_genome.txt 19
+
+python hcf.py HyperNEAT_Symmetry.txt 1 0 1 HyperNEAT_Symmetry_genome.txt 19
